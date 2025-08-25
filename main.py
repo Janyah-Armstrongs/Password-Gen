36d2875787de1e1b6481e222e3cfc119e8e9dc5d
Janyah-Armstrongs <Janyaharmstrong091406@gmail.com>
2025-08-25 17:57:50 -0400

    password generator

---
+++ main.py
@@ -0,0 +1,12 @@
+import random
+import string
+
+# Password Generator
+def generate_password(length=12):
+    characters = string.ascii_letters + string.digits + string.punctuation
+    return "".join(random.choice(characters) for _ in range(length))
+
+print("\n--- Password Generator ---")
+length = int(input("Enter password length: "))
+password = generate_password(length)
+print(f"Your new password is: {password}")
